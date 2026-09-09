cbed74d3a73c0b35bb30a5b87502c49603ab84fa
Informatica <informatica@alumno.local>
2026-09-09 11:12:03 -0600

    ft/Se agrego el Senemanager y el alertINformation

---
+++ gestion-trabajadores-hrcore/src/org/hrcore/system/utils/SceneManager.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.hrcore.system.utils;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+    
+    private static SceneManager instanciaSceneManager;
+    private Stage stagePrincipal;
+    
+    private SceneManager(){
+        
+    }
+    
+    public void changeScene(Scene scene){
+        try{
+            if(stagePrincipal != null && scene != null) {
+                stagePrincipal.setScene(scene);
+                stagePrincipal.sizeToScene(); // Ajusta el tamaño de la ventana al contenido
+                stagePrincipal.show();
+            } else {
+                System.out.println("Error: La ventana principal o la escena son nulas.");
+            }
+        } catch (NullPointerException objetoNulo) {
+            System.out.println("Error: Objeto nulo al cambiar de escena.");
+            objetoNulo.printStackTrace();
+        } catch (Exception errorPadre) {
+            System.out.println("Error general al cambiar de escena: " + errorPadre.getMessage());
+            errorPadre.printStackTrace();
+        }
+    }
+    
+        public static SceneManager getInstanciaSceneManager() {
+        if (instanciaSceneManager == null) {
+            instanciaSceneManager = new SceneManager();
+        }
+        return instanciaSceneManager;
+    }
+    
+    // Getters y Setters para la ventana principal
+    public Stage getStagePrincipal() {
+        return stagePrincipal;
+    }
+    
+    public void setStagePrincipal(Stage stagePrincipal) {
+        this.stagePrincipal = stagePrincipal;
+    }
+}
+
